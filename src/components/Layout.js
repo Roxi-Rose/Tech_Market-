2f7c681c9cf59f1a9b3bd4145798f91a32dbfa26
hhabiiba <hbmdhb.2003@gmail.com>
2023-11-28 20:52:09 -0800

    feat:added header,footer,layout

---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import '../App.css';
+import Header from './Header';
+import Footer from './Footer'; 
+
+function Layout() {
+  return (
+    <section className="layout">
+     <Header/>
+     <Footer/>
+    </section>
+  );
+}
+
+export default Layout;
