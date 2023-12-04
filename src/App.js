16e3a442bbdaf47652f8b551bd681719d34dff81
zozosy <zoubaida.taher17@gmail.com>
2023-12-04 03:27:16 -0800

    feat: added auction features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import Layout from './components/Layout';
-<<<<<<< HEAD
 import Auction from './components/auction'
-=======
 import Cart from './components/Cart';
->>>>>>> 031af9f250d67ecf0bbff6fc2349976b446ee419
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-<<<<<<< HEAD
-           <Auction />
-
-     {/* <Layout /> */}
-=======
+      {/* <Auction /> */}
       <Layout />
       {/* <Cart/> */}
->>>>>>> 031af9f250d67ecf0bbff6fc2349976b446ee419
     </div>
   );
 }
