5c464d06e1ada6f8c6ebd727b537ac522ca14363
Abdul-KMC <mfnadwi0522@gmail.com>
2023-12-08 18:23:03 +0530

    fix: updated routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import Auction from './components/auction'
+import Auction from './components/auction';
 import Cart from './components/Cart';
-import './App.css';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import ProductDetails from './components/ProductDetails';
-
-
-
+import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the API endpoint
+    fetch('https://6566ef4764fcff8d730f588d.mockapi.io/web')
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path = '/' element={<Layout/>}/>
-      <Route path = '/Cart' element = {<Cart/>}/>
-      <Route path = '/Detail' element = {<ProductDetails/>}/>
-      <Route path = '/Auction' element = {<Auction/>}/>
-    
-    </Routes>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+      <Route path='/' element={<Layout products={products} />} />
+        <Route path='/auction' element={<Auction />}></Route>
+        <Route path='/details/:id' element={<ProductDetails products={products} />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </Router>
   );
 }
 
